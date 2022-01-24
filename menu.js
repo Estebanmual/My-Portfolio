6415c49f991d5f4b8129a53dbf5dd73df5187147
Estebanmual <esteban.munozal@gmail.com>
2022-01-24 12:27:40 -0600

    Add js linter

--- menu.js
+++ menu.js
@@ -1,46 +1,43 @@
-
-
 /*
   Close mobile menu
 */
+function makeScrollable() {
+  const body = document.querySelector('body');
+  body.classList.remove('notScrollable');
+  body.classList.add('scrollable');
+}
+
 function closeMenu() {
-  let menuOptions = document.querySelector('.mobile-menu');
+  const menuOptions = document.querySelector('.mobile-menu');
   menuOptions.classList.remove('visible');
   menuOptions.classList.add('invisible');
   makeScrollable();
 }
 
-function makeScrollable() {
-  let body = document.querySelector('body');
-  body.classList.remove('notScrollable');
-  body.classList.add('scrollable');
-}
-
 function makeNotScrollable() {
-  let body = document.querySelector('body');
+  const body = document.querySelector('body');
   body.classList.remove('scrollable');
   body.classList.add('notScrollable');
 }
 
-
 /*
   Open the mobile menu
 */
-let btnMenu = document.getElementById('menu-options');
-btnMenu.addEventListener('click', e => {
-  let menuOptions = document.querySelector('.mobile-menu');
+const btnMenu = document.getElementById('menu-options');
+btnMenu.addEventListener('click', () => {
+  const menuOptions = document.querySelector('.mobile-menu');
   menuOptions.classList.remove('invisible');
   menuOptions.classList.add('visible');
-  window.scrollTo(0,0);
-  makeNotScrollable()
+  window.scrollTo(0, 0);
+  makeNotScrollable();
 });
 
-let closeBtn = document.querySelector('.mobile-menu button');
-closeBtn.addEventListener('click', e => {
+const closeBtn = document.querySelector('.mobile-menu button');
+closeBtn.addEventListener('click', () => {
   closeMenu();
 });
 
-document.addEventListener('click', e => {
+document.addEventListener('click', (e) => {
   if (e.target.matches('.mobile-menu ul li a')) {
     closeMenu();
     makeScrollable();
